68a6b404843a2262325e05ec8286a9067908b265
Hunorabraham <hunor335@gmail.com>
2024-12-16 16:54:43 +0100

    collision fuckery

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -5,24 +5,9 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
 	move_and_slide()
 	if(is_on_wall()):
+		print_debug(get_slide_collision_count())
 		for i in get_slide_collision_count():
 			var collision = get_slide_collision(i)
-			print("Collided with: ", collision.get_collider().name)
+			print_debug("Collided with: ", instance_from_id(collision.get_collider().get_instance_id()))
