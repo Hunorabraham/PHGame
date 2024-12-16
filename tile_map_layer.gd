3afd7cf1241a0ced6d3a5bd357b08a94eaee6ba5
Hunorabraham <hunor335@gmail.com>
2024-12-16 16:00:01 +0100

    collision, map loading

--- tile_map_layer.gd
+++ tile_map_layer.gd
@@ -7,23 +7,23 @@ func _ready() -> void:
 	# replace with actual tokenisation function later, input needs to be spaced out for now
 	# hard coded for now
 	#load room
-	var file = FileAccess.open("res://data//Room1.txt", FileAccess.READ)
-	var content = file.get_as_text()
+	loadRoom(1)
+	
+
+func loadRoom(id: int)->void:
+	var file = FileAccess.open("res://data//Room%d.txt" % id, FileAccess.READ)
+	var content = file.get_as_text(true)
 	for line in content.split("\n",false):
 		var dataPerLine = line.split(":",false)
 		var coords = parseVector(dataPerLine[0])
 		match dataPerLine[1]:
 			"floor":
 				#add floortile
-				self.set_cell(coords, -1,Vector2i(0,0),0)
-				pass
-			"transition":
+				print_debug("vittu")
+				self.set_cell(coords, 0,Vector2i(0,0),0)
+			"transit":
 				#add transition
-				self.set_cell(coords, -1,Vector2i(1,0),0)
-				pass
-	
-	#for tile in self.get_used_cells():
-	#	print_debug(tile,self.get_cell_atlas_coords(tile), self.get_cell_source_id(tile))
+				self.set_cell(coords, 0,Vector2i(1,0),0)
 
 func parseVector(source :String) -> Vector2i:
 	var nums = source.split(",")
